815f2cf4ae837b2626e8787376438ed14b847c78
Sanjeet Kumar <sanjeetkum960@gmail.com>
2024-09-09 23:38:32 +0530

    all define

--- JS code 1 lecture/index.js
+++ JS code 1 lecture/index.js
@@ -1,4 +1,4 @@
-console.log("Sanjeet kumar")
+/*console.log("Sanjeet kumar")
 console.log("my name is sanjeet kumar")
 console.log("Maharana Pratap was born to Udai Singh II of Mewar and Jaiwanta Bai in 1540, the year in which Udai Singh ascended to the throne after defeating Vanvir Singh.[5][6][7] His younger brothers were Shakti Singh, Vikram Singh and Jagmal Singh. Pratap also had two stepsisters: Chand Kanwar and Man Kanwar. His chief consort was Ajabde Bai Punwar of Bijolia.[8] Their eldest son was Amar Singh I.[9] He belonged to the royal family of Mewar.[10] After the death of Udai Singh in 1572, Rani Dheer Bai Bhatiyani wanted her son Jagmal to succeed him[11] but senior courtiers preferred Pratap, as the eldest son, to be their king. The desire of the nobles prevailed and Pratap ascended the throne as Maharana Pratap, the 54th ruler of Mewar in the line of the Sisodia Rajputs.[12] He was crowned in Gogunda on the auspicious day of Holi. Jagmal swore revenge and left for Ajmer, to join the armies of Emperor Akbar. He was given the town of Jahazpur as a Jagir as a gift in return for his help.[1")
 console.log("The Siege of Chittorgarh in 1567-1568 had led to the loss of the fertile eastern belt of Mewar to the Mughals. However, the rest of the wooded and hilly kingdom in the Aravalli range was still under the control of Maharana Pratap. Mughal Emperor Akbar was intent on securing a stable route to Gujarat through Mewar; when Pratap Singh was crowned king (Maharana) in 1572, Akbar sent a number of envoys, including one by Raja Man Singh I of Amer, entreating him to become a vassal like many other rulers in Rajputana. When Pratap refused to personally submit to Akbar and several attempts to diplomatically settle the issue failed, war became inevitable.[16][17]")
@@ -13,7 +13,7 @@ prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.log(accountEmail);
+/*console.log(accountEmail);
 console.log(accountId);
 console.log(accountPassword);
 console.table([accountId,accountPassword,accountEmail,accountState,accountDistric]);
@@ -45,3 +45,127 @@ console.log(4*6);
 console.log(4**6);
 console.log(4+6);
 console.log(24/4);
+let str1="sanjeet"
+let str2=" kumar"
+let str3 =str1+str2
+console.log(str3);
+//Console.log("1"+2);
+//Console.log(1+"2");
+ //console.log("1"+2+"2");
+ console.log(2>1);
+ console.log(2<1);
+ console.log(2>=1);
+ console.log("2">1);
+ console.log("02">1);
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null<0);
+ console.log(null>=0);
+ //memory
+ //stakr(primitive), heap(nonprimitive)
+ let myName ="Sanjeet kumar"
+ let otherName =myName
+ otherName ="bittu yadav"
+ console.log(myName);
+ console.log(otherName);
+let userOne ={
+    email :"sanj@843.gamail com",
+    upi:"egr@ybl"
+
+ }
+ let userTwo ={
+    email:"643trygf@dggd",
+    upi :"sgd@ybl"
+ }
+let userTwo = userOne
+userTwo.email ="bittu420@gail.com"
+    console.log(userOne.email);
+    console.log(userTwo.email);
+    const name ="Sanjeet kumar"
+    const repoCount = 50
+    console.log(name,repoCount,"value");
+    console.log(`hello my name is ${name} my repo count is ${repoCount}`);
+    const newGame = new String("sanjeet")
+    console.log(newGame.charAt(2));
+    console.log(newGame.indexOf('j'));
+    console.log(newGame.toLowerCase ());
+    console.log(newGame.toUpperCase());
+    console.log(newGame.length);
+    const newString = newGame.substring(0,4)
+    console.log(newString);
+    const otherString = newGame.slice(-8,4)
+    console.log(otherString);
+    const otherStringOne ="      sanjeet     "
+    console.log(otherStringOne);
+    console.log(otherStringOne.trim());
+const url ="hptts//:sanjeet.com@bittu%20yadav"
+console.log(url.replace('%20' ,'-'));
+console.log(url.includes("sanjeet"));
+console.log(url.includes("surya"));
+ console.log(newGame.split('-'));*/
+ /*const score = 400
+ console.log(score);
+ const balance =  new Number(100)
+ console.log(balance);
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(2));
+ const otherNumber = 1123.347
+ console.log(otherNumber.toPrecision(3));
+ const hundred =100000
+ console.log(hundred);
+ console.log(hundred.toLocaleString());
+ console.log(hundred.toLocaleString('en-IN'));
+ //+++++++++++++++++++++++++++++++MATH++++++++++++++++++++++
+ /*console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.1));
+ console.log(Math.ceil(5.1));
+ console.log(Math.floor(4.9));
+ console.log(Math.min(6,2,8,3,9,6,4,2,28,));
+ console.log(Math.max(64,7,0,36,82.));
+ console.log(Math.random());
+ console.log(Math.random()*10 +1);
+ const min =10
+ const max=20
+ console.log(Math.floor(Math.random()*(max-min+1)) +min);
+// +++++++++++++++++++DATE++++++++++++++++++++++++++++++++++++++//
+/*let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+let myCreateDate = new Date(2023,0,23)
+console.log(myCreateDate);
+console.log(myCreateDate.toDateString());
+//let myCreateDatePin = new Date(2023,0,23,3,5)
+console.log(myCreateDatePin.toLocaleDateString());
+let myCreateDatePinYear = new Date("2024-01-23")
+console.log(myCreateDatePinYear.toLocaleDateString());
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreateDatePin.getDate());
+//let newDate = new Date()
+//console.log(newDate.getMonth());
+//++++++++++++++++++++++++++++++++++++++++Array+++++++++++++++++++++++++++++++++
+/*let newArr = [0,1,2,3,4,5,6,7, true, "sanjeet kumar" ]
+console.table(newArr);
+const Heroes =["thor","loki","iron man",]
+console.log(Heroes);
+const myArr2 = new Array(1,2,3,4,5,6)
+console.log(myArr2[3]);
+myArr2.push(8)
+myArr2.pop()
+myArr2.unshift(0)
+myArr2.shift()
+console.log(myArr2);
+console.log(myArr2.includes(10));
+console.log(myArr2.includes(5));
+console.log(myArr2.indexOf(100));
+console.log("A ", myArr2);
+const myn1 = myArr2.slice(1,3)
+console.log(myn1);
+console.log("B",myArr2);
+//console.log("c",myArr2);
+const myn2 = myArr2.splice(1, 3)
+console.log("C",myArr2)
+console.log(myn2);*/
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("sanjeet kumar")
+Console.log("1" + 2);
+Console.log(1 + "2");
+ console.log("1" + 2 +"2");
