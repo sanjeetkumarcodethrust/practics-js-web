0b6adaef54a3c8929088b7a0fd0402bc25d808e3
Sanjeet Kumar <sanjeetkum960@gmail.com>
2024-09-23 23:53:51 +0530

    fuction in js

---
+++ JS code 1 lecture/fuction.js
@@ -0,0 +1,75 @@
+/*console.log("s")
+console.log("a")
+console.log("n")
+console.log("j")
+console.log("e")
+console.log("e")
+console.log("t")*/
+
+/*function saymyname(){
+    console.log("s")
+console.log("a")
+console.log("n")
+console.log("j")
+console.log("e")
+console.log("e")
+console.log("t")
+}
+saymyname()*/
+
+/*function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+    
+function addTwoNumbers(number1,number2){
+    let result=number1+number2
+    console.log("sanjeet");
+    return result
+    
+
+}
+
+    
+
+
+const result = addTwoNumbers(87,82)
+console.log("result:",result);
+
+
+function loginusermassage(username){
+    if(username ===undefined){
+        console.log("please enter the login");
+        return
+
+    }
+    return `${username} just logged in`
+
+}
+console.log(loginusermassage());
+function calculatecardpricr(val1,val2,...num1){
+    return num1
+//val1 and two take no.200,400
+}
+console.log(calculatecardpricr(2));
+console.log(calculatecardpricr(200,400 ,500,2000));
+
+
+const user = {
+    username:"sanjeet",
+    price:"199"
+}
+function HandleObject(anyobject){
+    console.log(`username is ${anyobject.username } and price is ${anyobject.price}`);
+}
+//HandleObject(user)
+HandleObject({
+    username:"sanjeet",
+    price:"399",
+})*/
+
+
+
+const mynewarrey = [266,870,635,5366,826,]
+function returnsecondvalue(getarrey){
+    return getarrey[0]
+}
+console.log(returnsecondvalue(mynewarrey));
